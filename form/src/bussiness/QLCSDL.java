86b56d99ad2a9483d7b6e61e865ae4446faade93
khoa <chronicle1951@gmail.com>
2020-01-02 21:04:07 +0700

    use database from remotemysql.com

--- form/src/bussiness/QLCSDL.java
+++ form/src/bussiness/QLCSDL.java
@@ -7,11 +7,13 @@ import java.sql.SQLException;
 
 
 public class QLCSDL {
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/nhasach?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	//private static final String JDBC_URL = "jdbc:mysql//localhost/nhasach?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	//private static final String JDBC_URL = "jdbc:mysql://localhost:3306/nhasach?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static final String JDBC_URL = "jdbc:mysql://remotemysql.com:3306/NUMK10OFLJ";
 	private static final String JDBC_DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
-	private static final String JDBC_USERNAME = "root";
-	private static final String JDBC_PASSWORD = "";
+//	private static final String JDBC_USERNAME = "root";
+//	private static final String JDBC_PASSWORD = "";
+	private static final String JDBC_USERNAME = "NUMK10OFLJ";
+	private static final String JDBC_PASSWORD = "FVKS2A5kuU";
 	
 	//Kết nối CSDL
 	public static Connection connect() throws SQLException, ClassNotFoundException 
